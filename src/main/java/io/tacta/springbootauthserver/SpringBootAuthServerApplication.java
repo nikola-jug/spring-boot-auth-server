15b58394204d3fee4296d841d7d69375a7da8daa
Nikola Jug <nixajug@gmail.com>
2026-03-22 09:05:33 +0100

    Initial commit

---
+++ src/main/java/io/tacta/springbootauthserver/SpringBootAuthServerApplication.java
@@ -0,0 +1,13 @@
+package io.tacta.springbootauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tacta/springbootauthserver/SpringBootAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tacta.springbootauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
